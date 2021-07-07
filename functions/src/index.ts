0be571f268127291cfe186301879f432e0196406
Abdulsalam Ibrahim <abdulsalamibrahim7@gmail.com>
2021-07-07 20:13:26 +0200

    changed import

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,12 +1,12 @@
 import * as functions from 'firebase-functions'
 import * as express from 'express'
-import { generateQrCode } from './controllers/entryController'
+import { getQrCodes } from './controllers/entryController'
 const app = express()
 
 app.get('/', (req, res) => {
-  return res.status(200).send('Success')
+  return res.status(200).send('Success')   
 })
 
-app.post('/addNumericCodes', generateQrCode)
+app.post('/addNumericCodes', getQrCodes)
 
 exports.qrcodeGen = functions.https.onRequest(app)
