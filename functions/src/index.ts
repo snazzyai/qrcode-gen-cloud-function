1d48ca0e9e834d8a8037cf06779172db455abe69
Abdulsalam Ibrahim <abdulsalamibrahim7@gmail.com>
2021-07-08 09:28:30 +0200

    changed routing

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -3,10 +3,6 @@ import * as express from 'express'
 import { getQrCodes } from './controllers/entryController'
 const app = express()
 
-app.get('/', (req, res) => {
-  return res.status(200).send('Success')   
-})
-
-app.post('/addNumericCodes', getQrCodes)
+app.post('/', getQrCodes)
 
 exports.qrcodeGen = functions.https.onRequest(app)
