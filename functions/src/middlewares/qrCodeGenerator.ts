97e4561063bcfef667fae69751c231430c25123d
Abdulsalam Ibrahim <abdulsalamibrahim7@gmail.com>
2021-07-08 09:32:01 +0200

    deleted middleware folder and modified json file

--- functions/src/middlewares/qrCodeGenerator.ts
+++
@@ -1,9 +0,0 @@
-import * as qrcode from 'qrcode'
-
-const generateQrCode = async (data: string): Promise<string> => {
-  const imageUrl = await qrcode.toDataURL(data);
-  const result = imageUrl
-  return result
-};
-
-export default generateQrCode
